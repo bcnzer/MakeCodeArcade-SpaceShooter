f276b3dae03223d17a5832a43904a0b4f2e0f20b
Ben Chartrand <bcnzer@users.noreply.github.com>
2021-02-13 19:33:12 +1300

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,126 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . 8 8 . . . . . . . 
+. . . . . . . 8 8 . . . . . . . 
+. . . . . . 8 8 8 8 . . . . . . 
+. . . . . . 8 8 8 8 . . . . . . 
+. . . . . 8 8 9 9 8 8 . . . . . 
+. . . . . 8 8 9 9 8 8 . . . . . 
+. . . . 8 8 8 9 9 8 8 8 . . . . 
+. . . 8 8 8 8 9 9 8 8 8 8 . . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . . 8 2 2 2 8 8 2 2 2 8 . . . 
+. . . . 2 5 2 . . 2 5 2 . . . . 
+. . . . 4 5 4 . . 4 5 4 . . . . 
+. . . . . 5 . . . . 5 . . . . . 
+. . . . . 5 . . . . 5 . . . . . 
+`;
+            case "image2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . c . . . . . . . . 
+. . . . . . . c . . . . . . . . 
+. . . . . . c c c . . . . . . . 
+. . . . . . c 9 c . . . . . . . 
+. . . . . . c 9 c . . . . . . . 
+. . . . . . c 9 c . . . . . . . 
+. . . . . . c c c . . . . . . . 
+. . . . . . 2 2 2 . . . . . . . 
+. . . . . . . 5 . . . . . . . . 
+. . . . . . . 5 . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":return img`
+. . . . . . . . . c c 8 . . . . 
+. . . . . . 8 c c c f 8 c c . . 
+. . . c c 8 8 f c a f f f c c . 
+. . c c c f f f c a a f f c c c 
+8 c c c f f f f c c a a c 8 c c 
+c c c b f f f 8 a c c a a a c c 
+c a a b b 8 a b c c c c c c c c 
+a f c a a b b a c c c c c f f c 
+a 8 f c a a c c a c a c f f f c 
+c a 8 a a c c c c a a f f f 8 a 
+. a c a a c f f a a b 8 f f c a 
+. . c c b a f f f a b b c c 6 c 
+. . . c b b a f f 6 6 a b 6 c . 
+. . . c c b b b 6 6 a c c c c . 
+. . . . c c a b b c c c . . . . 
+. . . . . c c c c c c . . . . . 
+`;
+            case "image3":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . c c c c . . 
+. c c c c c . c c c c c f c c . 
+c c a c c c c c 8 f f c f f c c 
+c a f a a c c a f f c a a f f c 
+c a 8 f a a c a c c c a a a a c 
+c b c f a a a a a c c c c c c c 
+c b b a a c f 8 a c c c 8 c c c 
+. c b b a b c f a a a 8 8 c c . 
+. . . . a a b b b a a 8 a c . . 
+. . . . c b c a a c c b . . . . 
+. . . . b b c c a b b a . . . . 
+. . . . b b a b a 6 a . . . . . 
+. . . . c b b b 6 6 c . . . . . 
+. . . . . c a 6 6 b c . . . . . 
+. . . . . . . c c c . . . . . . 
+`;
+            case "image5":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . c c c . . . . . . 
+. . . . . . a b a a . . . . . . 
+. . . . . c b a f c a c . . . . 
+. . . . c b b b f f a c c . . . 
+. . . . b b f a b b a a c . . . 
+. . . . c b f f b a f c a . . . 
+. . . . . c a a c b b a . . . . 
+. . . . . . c c c c . . . . . . 
+. . . . . . . c . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image6":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . c c . . . . . . . . 
+. . . . c a f b c . . . . . . . 
+. . . . b f f b c c . . . . . . 
+. . . a a f b a b a c . . . . . 
+. . . c a c b b f f b . . . . . 
+. . . . b f f b f a b . . . . . 
+. . . . a f f b b b a . . . . . 
+. . . . . a b b c c . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,141 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    missile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . c . . . . . . . . 
+        . . . . . . . c . . . . . . . . 
+        . . . . . . c c c . . . . . . . 
+        . . . . . . c 9 c . . . . . . . 
+        . . . . . . c 9 c . . . . . . . 
+        . . . . . . c 9 c . . . . . . . 
+        . . . . . . c c c . . . . . . . 
+        . . . . . . 2 2 2 . . . . . . . 
+        . . . . . . . 5 . . . . . . . . 
+        . . . . . . . 5 . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySpaceship, 0, -45)
+    music.pewPew.play()
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    game.over(false, effects.dissolve)
+})
+sprites.onDestroyed(SpriteKind.Enemy, function (sprite) {
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.trail, 100)
+    sprite.destroy()
+})
+let spaceRock: Sprite = null
+let rockNumber = 0
+let missile: Sprite = null
+let mySpaceship: Sprite = null
+game.splash("Ben's Space Shooter", "Asteroids incoming!")
+scene.setBackgroundColor(15)
+effects.starField.startScreenEffect()
+mySpaceship = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 8 8 . . . . . . . 
+    . . . . . . . 8 8 . . . . . . . 
+    . . . . . . 8 8 8 8 . . . . . . 
+    . . . . . . 8 8 8 8 . . . . . . 
+    . . . . . 8 8 9 9 8 8 . . . . . 
+    . . . . . 8 8 9 9 8 8 . . . . . 
+    . . . . 8 8 8 9 9 8 8 8 . . . . 
+    . . . 8 8 8 8 9 9 8 8 8 8 . . . 
+    . . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+    . . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+    . . . 8 2 2 2 8 8 2 2 2 8 . . . 
+    . . . . 2 5 2 . . 2 5 2 . . . . 
+    . . . . 4 5 4 . . 4 5 4 . . . . 
+    . . . . . 5 . . . . 5 . . . . . 
+    . . . . . 5 . . . . 5 . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySpaceship)
+mySpaceship.setStayInScreen(true)
+mySpaceship.setPosition(80, 110)
+game.onUpdateInterval(500, function () {
+    rockNumber = randint(0, 4)
+    if (rockNumber == 0) {
+        spaceRock = sprites.create(img`
+            . . . . . . . . . c c 8 . . . . 
+            . . . . . . 8 c c c f 8 c c . . 
+            . . . c c 8 8 f c a f f f c c . 
+            . . c c c f f f c a a f f c c c 
+            8 c c c f f f f c c a a c 8 c c 
+            c c c b f f f 8 a c c a a a c c 
+            c a a b b 8 a b c c c c c c c c 
+            a f c a a b b a c c c c c f f c 
+            a 8 f c a a c c a c a c f f f c 
+            c a 8 a a c c c c a a f f f 8 a 
+            . a c a a c f f a a b 8 f f c a 
+            . . c c b a f f f a b b c c 6 c 
+            . . . c b b a f f 6 6 a b 6 c . 
+            . . . c c b b b 6 6 a c c c c . 
+            . . . . c c a b b c c c . . . . 
+            . . . . . c c c c c c . . . . . 
+            `, SpriteKind.Enemy)
+    } else if (rockNumber == 1) {
+        spaceRock = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . c c c c . . 
+            . c c c c c . c c c c c f c c . 
+            c c a c c c c c 8 f f c f f c c 
+            c a f a a c c a f f c a a f f c 
+            c a 8 f a a c a c c c a a a a c 
+            c b c f a a a a a c c c c c c c 
+            c b b a a c f 8 a c c c 8 c c c 
+            . c b b a b c f a a a 8 8 c c . 
+            . . . . a a b b b a a 8 a c . . 
+            . . . . c b c a a c c b . . . . 
+            . . . . b b c c a b b a . . . . 
+            . . . . b b a b a 6 a . . . . . 
+            . . . . c b b b 6 6 c . . . . . 
+            . . . . . c a 6 6 b c . . . . . 
+            . . . . . . . c c c . . . . . . 
+            `, SpriteKind.Enemy)
+    } else if (rockNumber == 2) {
+        spaceRock = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . c c c . . . . . . 
+            . . . . . . a b a a . . . . . . 
+            . . . . . c b a f c a c . . . . 
+            . . . . c b b b f f a c c . . . 
+            . . . . b b f a b b a a c . . . 
+            . . . . c b f f b a f c a . . . 
+            . . . . . c a a c b b a . . . . 
+            . . . . . . c c c c . . . . . . 
+            . . . . . . . c . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+    } else {
+        spaceRock = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . c c . . . . . . . . 
+            . . . . c a f b c . . . . . . . 
+            . . . . b f f b c c . . . . . . 
+            . . . a a f b a b a c . . . . . 
+            . . . c a c b b f f b . . . . . 
+            . . . . b f f b f a b . . . . . 
+            . . . . a f f b b b a . . . . . 
+            . . . . . a b b c c . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+    }
+    spaceRock.setPosition(randint(10, 150), 0)
+    spaceRock.setVelocity(randint(-50, 50), randint(10, 50))
+})
